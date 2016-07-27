0b7fdb1414281e675e1719130799d0a7563dc101
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-07-27 14:42:23 +0900

    add the infrastructure to inject an object to each window

--- bootstrap.js
+++ bootstrap.js
@@ -4,19 +4,67 @@
 
 /* eslint-env commonjs */
 /* global Components: false */
+
 "use strict";
 
 const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
 // Bootstrap Addon Reason Constants:
 // const APP_STARTUP = 1;
-// const APP_SHUTDOWN = 2;
+const APP_SHUTDOWN = 2;
 // const ADDON_ENABLE = 3;
 // const ADDON_DISABLE = 4;
 // const ADDON_INSTALL = 5;
 // const ADDON_UNINSTALL = 6;
 // const ADDON_UPGRADE = 7;
 // const ADDON_DOWNGRADE = 8;
+
+const {Services} = Cu.import("resource://gre/modules/Services.jsm", {});
+
+const SetupHelper = {
+  /**
+   * @param {Window} aDomWindow
+   * @returns {void}
+   */
+  setup(aDomWindow) {
+    const windowType = aDomWindow.document.
+                     documentElement.getAttribute("windowtype");
+    // If this isn't a browser window then abort setup.
+    if (windowType !== "navigator:browser") {
+      return;
+    }
+  },
+
+  /**
+   * @param {Window} aDomWindow
+   * @returns {void}
+   */
+  teardown(/*aDomWindow*/) {}, // eslint-disable-line no-empty-function
+};
+
+// nsIWindowMediatorListener
+const WindowListener = {
+
+  /**
+   * @param {Window} aXulWindow
+   * @returns {void}
+   */
+  onOpenWindow(aXulWindow) {
+    const domWindow = aXulWindow.QueryInterface(Ci.nsIInterfaceRequestor) // eslint-disable-line new-cap
+                    .getInterface(Ci.nsIDOMWindow);
+
+    // Wait finish loading
+    domWindow.addEventListener("load", function onLoad(/* aEvent */) {
+      domWindow.removeEventListener("load", onLoad, false);
+      SetupHelper.setup(domWindow);
+    }, false);
+  },
+
+  onCloseWindow(/*aXulWindow*/) {}, // eslint-disable-line no-empty-function
+
+  onWindowTitleChange(/*aWindow, aNewTitle*/) {}, // eslint-disable-line no-empty-function
+};
+
 
 /**
  * bootstrapped addon interfaces
@@ -26,6 +74,13 @@ const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
  * @returns {void}
  */
 function startup(aData, aReason) { // eslint-disable-line no-unused-vars
+  Services.wm.addListener(WindowListener);
+
+  const windows = Services.wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    const domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow); // eslint-disable-line new-cap
+    SetupHelper.setup(domWindow);
+  }
 }
 
 /**
@@ -34,6 +89,18 @@ function startup(aData, aReason) { // eslint-disable-line no-unused-vars
  * @returns {void}
  */
 function shutdown(aData, aReason) { // eslint-disable-line no-unused-vars
+  Services.wm.removeListener(WindowListener);
+
+  // if the application is shutdown time, we don't have to call these step.
+  if (aReason === APP_SHUTDOWN) {
+    return;
+  }
+
+  const windows = Services.wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    const domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow); // eslint-disable-line new-cap
+    SetupHelper.teardown(domWindow);
+  }
 }
 
 /**
