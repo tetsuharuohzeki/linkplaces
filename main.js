9a7c5a39faa86267b65db2c57a158cd03fa10500
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-11 02:22:14 +0900

    Add more informations to package.json

---
+++ main.js
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* eslint-env commonjs */
+
+"use strict";
+
+/**
+ *  https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials/Listening_for_load_and_unload
+ *
+ *  @param  { { loadReason: string, } } options
+ *  @param  {Function}  callbacks
+ *  @return {void}
+ *
+ */
+exports.main = function (options, callbacks) { // eslint-disable-line no-unused-vars
+};
+
+/**
+ *  https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials/Listening_for_load_and_unload
+ *
+ *  @param  {string}  reason
+ *  @return {void}
+ */
+exports.onUnload = function (reason) { // eslint-disable-line no-unused-vars
+};
