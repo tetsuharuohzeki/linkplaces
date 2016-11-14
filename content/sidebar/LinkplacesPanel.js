9a2428b1d940dc851fa3d7d19cb35f1cbf3f5bd0
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-15 04:58:52 +0900

    Fix source type of LinkplacesPanel

--- content/sidebar/LinkplacesPanel.js
+++ content/sidebar/LinkplacesPanel.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/**eslint-env commonjs */
-
 "use strict";
 
 // eslint-disable-next-line no-unused-vars
@@ -15,8 +13,11 @@ const Cu = Components.utils;
 const { PlacesUtils } = Cu.import("resource://gre/modules/PlacesUtils.jsm", {});
 const { PlacesUIUtils } = Cu.import("resource:///modules/PlacesUIUtils.jsm", {});
 
-// eslint-disable-next-line no-implicit-globals
-function LinkplacesPanel(aWindow) {
+/**
+ *  @constructor
+ *  @param  {XulWindow} aWindow
+ */
+function LinkplacesPanel(aWindow) { // eslint-disable-line no-implicit-globals
   this.window = aWindow;
   this.treeView = null;
   this.ctxMenu = null;
