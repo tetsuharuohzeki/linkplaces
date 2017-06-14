2db7d1478a71e398995705a0e51ffc55b12c7f3f
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-06-14 17:10:37 +0900

    Make webextension/background/Bookmark.js ts-check

--- src/webextension/background/Bookmark.js
+++ src/webextension/background/Bookmark.js
@@ -1,11 +1,20 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-/* @flow */
 
-/*global browser: false */
+// @ts-check
+/* eslint-env webextensions */
 
-export async function createBookmarkItem(url /* :string*/, title /* :string*/) /* :Promise<webext$bookmarks$BookmarkTreeNode> */ {
+// @ts-ignore
+import * as _ from '../../../typings/webext/index'; // eslint-disable-line no-unused-vars
+import { BookmarkTreeNode } from '../../../typings/webext/bookmarks'; // eslint-disable-line no-unused-vars
+
+/**
+ *  @param {string} url
+ *  @param {string} title
+ *  @return {Promise<BookmarkTreeNode>}
+ */
+export async function createBookmarkItem(url, title) {
     // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/bookmarks/create
     // Save to "Other Bookmarks" if there is no `parentId`
     const result = browser.bookmarks.create({
