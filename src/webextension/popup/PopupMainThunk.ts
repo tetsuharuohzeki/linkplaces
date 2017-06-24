280872c79d477dd034b89fe6be22b1df4d3270d2
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-06-25 07:32:06 +0900

    Use constant insteaf direct value

--- src/webextension/popup/PopupMainThunk.ts
+++ src/webextension/popup/PopupMainThunk.ts
@@ -1,7 +1,11 @@
 import { Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 
-import { MSG_TYPE_OPEN_ORGANIZE_WINDOW_FROM_POPUP } from '../background/IpcMsg';
+import {
+    MSG_TYPE_OPEN_ORGANIZE_WINDOW_FROM_POPUP,
+    MSG_TYPE_OPEN_URL_FROM_POPUP,
+    MSG_TYPE_OPEN_SIDEBAR_FROM_POPUP,
+} from '../background/IpcMsg';
 import { getLinkSchemeType } from '../shared/Bookmark';
 import { Channel } from '../shared/Channel';
 
@@ -18,7 +22,7 @@ export type ThunkArguments = Readonly<{
 export function openItem(id: string, url: string): ThunkAction<Promise<void>, PopupMainState, ThunkArguments> {
     return function openItemActual(_dispatch: Dispatch<PopupMainState>, _1: PopupMainState, dependencies: ThunkArguments): Promise<void> {
         if (!getLinkSchemeType(url).isPrivileged) {
-            dependencies.channel.postOneShotMessage('linkplaces-open-url-from-popup', { id, url });
+            dependencies.channel.postOneShotMessage(MSG_TYPE_OPEN_URL_FROM_POPUP, { id, url });
         }
 
         return Promise.resolve().then(() => {
@@ -29,7 +33,7 @@ export function openItem(id: string, url: string): ThunkAction<Promise<void>, Po
 
 export function openSidebar(): ThunkAction<Promise<void>, PopupMainState, ThunkArguments> {
     return function openItemActual(dispatch: Dispatch<PopupMainState>, _1: PopupMainState, dependencies: ThunkArguments): Promise<void> {
-        dependencies.channel.postOneShotMessage('linkplaces-open-classic-sidebar-from-popup', null);
+        dependencies.channel.postOneShotMessage(MSG_TYPE_OPEN_SIDEBAR_FROM_POPUP, null);
 
         dispatch(createOpenSidebarAction());
 
