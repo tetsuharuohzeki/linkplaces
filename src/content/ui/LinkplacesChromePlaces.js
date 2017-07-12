e2bd863b09dd9161bb13c2b496e178c34c4c81d3
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-07-12 11:19:21 +0900

    Merge content/ui/contextmenu.js into LinkplacesChromePlaces.js

--- src/content/ui/LinkplacesChromePlaces.js
+++ src/content/ui/LinkplacesChromePlaces.js
@@ -2,10 +2,69 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { ContextMenuItem } from "./contextmenu";
-
 const ID_PLACES_CTX_MENU = "placesContext";
 const ID_CTX_SAVE_ITEMS = "linkplaces-placesCtx-saveItems";
+
+class ContextMenuItem {
+  constructor(win, param) {
+    this._dom = null;
+    this._onCommand = null;
+
+    Object.seal(this);
+    this._init(win, param);
+  }
+
+  destroy() {
+    this._finalize();
+
+    this._onCommand = null;
+    this._dom = null;
+  }
+
+  _init(win, { id, label, onCommand, mountpoint, attr}) {
+    const dom = win.document.createElement("menuitem");
+    dom.setAttribute("id", id);
+    dom.setAttribute("label", label);
+    dom.addEventListener("command", this, false);
+
+    if (!!attr) {
+      for (let [key, value] of attr.entries()) { //eslint-disable-line prefer-const
+        dom.setAttribute(key, value);
+      }
+    }
+
+    this._dom = dom;
+    this._onCommand = onCommand;
+
+    const mp = win.document.querySelector(mountpoint);
+    mp.parentNode.insertBefore(dom, mp);
+  }
+
+  _finalize() {
+    this._dom.remove();
+    this._dom.removeEventListener("command", this, false);
+  }
+
+  handleEvent(event) {
+    switch (event.type) {
+      case "command":
+        this._onCommand(event);
+        break;
+    }
+  }
+
+  finalize() {
+    this._dom.remove();
+
+    this._dom.removeEventListener("command", this, false);
+    this._onCommand = null;
+    this._dom = null;
+  }
+
+  onCommand() {
+    this._onCommand();
+  }
+}
 
 export class LinkplacesChromePlaces {
 
