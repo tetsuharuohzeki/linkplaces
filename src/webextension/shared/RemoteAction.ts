e70406286bd82f02f0032bfefa01636c73a90c26
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-08-17 03:52:53 +0900

    Remove needless '-from-popup' postfix

--- src/webextension/shared/RemoteAction.ts
+++ src/webextension/shared/RemoteAction.ts
@@ -1,8 +1,8 @@
 export const MSG_TYPE_OPEN_URL = 'linkplaces-open-tab';
 export const MSG_TYPE_OPEN_URL_RESULT = 'linkplaces-open-tab-result';
-export const MSG_TYPE_OPEN_URL_FROM_POPUP = 'linkplaces-open-url-from-popup';
-export const MSG_TYPE_OPEN_SIDEBAR_FROM_POPUP = 'linkplaces-open-classic-sidebar-from-popup';
-export const MSG_TYPE_OPEN_ORGANIZE_WINDOW_FROM_POPUP = 'linkplaces-open-classic-organize-window-from-popup';
+export const MSG_TYPE_OPEN_URL_FROM_POPUP = 'linkplaces-open-url';
+export const MSG_TYPE_OPEN_SIDEBAR_FROM_POPUP = 'linkplaces-open-classic-sidebar';
+export const MSG_TYPE_OPEN_ORGANIZE_WINDOW_FROM_POPUP = 'linkplaces-open-classic-organize-window';
 
 interface RemoteActionBase {
     type: string;
