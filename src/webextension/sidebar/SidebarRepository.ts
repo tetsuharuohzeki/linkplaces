f18ec9cda267e852a2206a5be43e2f59955ea8de
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-09-03 04:16:53 +0900

    Fmt SidebarRepository

--- src/webextension/sidebar/SidebarRepository.ts
+++ src/webextension/sidebar/SidebarRepository.ts
@@ -7,45 +7,45 @@ import { Repository } from '../shared/Repository';
 
 export class SidebarRepository implements Repository<Array<BookmarkTreeNode>>, Observer<Array<BookmarkTreeNode>> {
 
-        static create(bookmarks: WebExtBookmarkService, _init: Array<BookmarkTreeNode>): SidebarRepository {
-            const s = new SidebarRepository();
+    static create(bookmarks: WebExtBookmarkService, _init: Array<BookmarkTreeNode>): SidebarRepository {
+        const s = new SidebarRepository();
 
-            const callback = () => {
-                getUnfiledBoolmarkFolder().then((list) => {
-                    s.next(list);
-                }).catch(console.error);
-            };
-            bookmarks.onChanged.addListener(callback);
-            // bookmarks.onChildrenReordered.addListener(callback); // unimplemted in Fireofxma
-            bookmarks.onMoved.addListener(callback);
-            bookmarks.onCreated.addListener(callback);
-            bookmarks.onRemoved.addListener(callback);
-            return s;
-        }
-
-        private _subject: Subject<Array<BookmarkTreeNode>>;
-
-        private constructor() {
-            this._subject = new Subject();
-        }
-
-        next(v: Array<BookmarkTreeNode>): void {
-            this._subject.next(v);
-        }
-
-        error(v: Array<BookmarkTreeNode>): void {
-            this._subject.error(v);
-        }
-
-        complete(): void {
-            this._subject.complete();
-        }
-
-        destroy(): void {
-            this._subject.unsubscribe();
-        }
-
-        asObservable(): Observable<Array<BookmarkTreeNode>> {
-            return this._subject;
-        }
+        const callback = () => {
+            getUnfiledBoolmarkFolder().then((list) => {
+                s.next(list);
+            }).catch(console.error);
+        };
+        bookmarks.onChanged.addListener(callback);
+        // bookmarks.onChildrenReordered.addListener(callback); // unimplemted in Fireofxma
+        bookmarks.onMoved.addListener(callback);
+        bookmarks.onCreated.addListener(callback);
+        bookmarks.onRemoved.addListener(callback);
+        return s;
     }
+
+    private _subject: Subject<Array<BookmarkTreeNode>>;
+
+    private constructor() {
+        this._subject = new Subject();
+    }
+
+    next(v: Array<BookmarkTreeNode>): void {
+        this._subject.next(v);
+    }
+
+    error(v: Array<BookmarkTreeNode>): void {
+        this._subject.error(v);
+    }
+
+    complete(): void {
+        this._subject.complete();
+    }
+
+    destroy(): void {
+        this._subject.unsubscribe();
+    }
+
+    asObservable(): Observable<Array<BookmarkTreeNode>> {
+        return this._subject;
+    }
+}
