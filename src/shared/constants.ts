f1d0c2d5103aeb47a7d4e4ef6822bda328a66867
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-02-27 00:33:57 +0900

    Remove needless ts-ignore

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,7 @@
-// @ts-ignore: this will be embedded by the linker.
-export const GIT_REVISION: string = process.env.GIT_REVISION;
+// this will be embedded by the linker.
+// tslint:disable-next-line:no-non-null-assertion
+export const GIT_REVISION: string = process.env.GIT_REVISION!;
 
-// @ts-ignore: this will be embedded by the linker.
-export const BUILD_DATE: string = process.env.BUILD_DATE;
+// this will be embedded by the linker.
+// tslint:disable-next-line:no-non-null-assertion
+export const BUILD_DATE: string = process.env.BUILD_DATE!;
