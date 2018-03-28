09c4cd9244a0414e6ed84d1fd2e2e8e0b8308f92
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-03-29 02:19:08 +0900

    export an async function from rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -33,111 +33,114 @@ IS_PRODUCTION_MODE: ${IS_PRODUCTION_MODE}
 
 // https://github.com/rollup/rollup/wiki/JavaScript-API
 // https://github.com/rollup/rollup/wiki/Command-Line-Interface
-module.exports = {
-    treeshake: true,
+module.exports = async function (_commandLineArgs) {
 
-    output: {
-        strict: true,
-        format: 'es',
-        exports: 'none',
+    return {
+        treeshake: true,
 
-        sourcemap: true,
-        globals: {
+        output: {
+            strict: true,
+            format: 'es',
+            exports: 'none',
+
+            sourcemap: true,
+            globals: {
+            },
         },
-    },
 
-    external: [
-    ],
+        external: [
+        ],
 
-    perf: false,
+        perf: false,
 
-    experimentalDynamicImport: true,
+        experimentalDynamicImport: true,
 
-    plugins: [
-        replaceImportWithGlobal({
-            'react': createDefaultExport('window.React'),
-            'react-dom': createDefaultExport('window.ReactDOM'),
-            'prop-types': createDefaultExport('window.PropTypes'),
-            'redux-thunk': createDefaultExport('window.ReduxThunk.default'),
+        plugins: [
+            replaceImportWithGlobal({
+                'react': createDefaultExport('window.React'),
+                'react-dom': createDefaultExport('window.ReactDOM'),
+                'prop-types': createDefaultExport('window.PropTypes'),
+                'redux-thunk': createDefaultExport('window.ReduxThunk.default'),
 
-            // I know these are pretty messy approach.
-            // But rxjs does not support properly TypeScript+rollup
-            'rxjs/BehaviorSubject': createModule([
-                createNamedExport('BehaviorSubject', 'window.Rx'),
-            ]),
-            'rxjs/Observable': createModule([
-                createNamedExport('Observable', 'window.Rx'),
-            ]),
-            'rxjs/observable/merge': createModule([
-                createNamedExport('merge', 'window.Rx.Observable'),
-            ]),
-            'rxjs/operators': createModule([
-                createNamedExport('filter', 'window.Rx.operators'),
-                createNamedExport('map', 'window.Rx.operators'),
-                createNamedExport('share', 'window.Rx.operators'),
-                createNamedExport('startWith', 'window.Rx.operators'),
-                createNamedExport('observeOn', 'window.Rx.operators'),
-            ]),
-            'rxjs/Subject': createModule([
-                createNamedExport('Subject', 'window.Rx'),
-            ]),
-            'rxjs/Subscription': createModule([
-                createNamedExport('Subscription', 'window.Rx'),
-            ]),
-            'rxjs/scheduler/animationFrame': createModule([
-                createNamedExport('animationFrame', 'window.Rx.Scheduler'),
-            ]),
-        }),
+                // I know these are pretty messy approach.
+                // But rxjs does not support properly TypeScript+rollup
+                'rxjs/BehaviorSubject': createModule([
+                    createNamedExport('BehaviorSubject', 'window.Rx'),
+                ]),
+                'rxjs/Observable': createModule([
+                    createNamedExport('Observable', 'window.Rx'),
+                ]),
+                'rxjs/observable/merge': createModule([
+                    createNamedExport('merge', 'window.Rx.Observable'),
+                ]),
+                'rxjs/operators': createModule([
+                    createNamedExport('filter', 'window.Rx.operators'),
+                    createNamedExport('map', 'window.Rx.operators'),
+                    createNamedExport('share', 'window.Rx.operators'),
+                    createNamedExport('startWith', 'window.Rx.operators'),
+                    createNamedExport('observeOn', 'window.Rx.operators'),
+                ]),
+                'rxjs/Subject': createModule([
+                    createNamedExport('Subject', 'window.Rx'),
+                ]),
+                'rxjs/Subscription': createModule([
+                    createNamedExport('Subscription', 'window.Rx'),
+                ]),
+                'rxjs/scheduler/animationFrame': createModule([
+                    createNamedExport('animationFrame', 'window.Rx.Scheduler'),
+                ]),
+            }),
 
-        // https://github.com/rollup/rollup-plugin-node-resolve
-        nodeResolve({
-            module: true,
-            main: true,
-            jsnext: true,
-            browser: true, // for browser
-            preferBuiltins: false, // linking for browser
+            // https://github.com/rollup/rollup-plugin-node-resolve
+            nodeResolve({
+                module: true,
+                main: true,
+                jsnext: true,
+                browser: true, // for browser
+                preferBuiltins: false, // linking for browser
 
-            // rollup does not have 'extensions' option,
-            // so we need to specify this option at here to import jsx file.
-            extensions: ['.mjs', '.js', '.jsx'],
-        }),
+                // rollup does not have 'extensions' option,
+                // so we need to specify this option at here to import jsx file.
+                extensions: ['.mjs', '.js', '.jsx'],
+            }),
 
-        // https://github.com/rollup/rollup-plugin-replace
-        replace({
-            exclude: [
-                'node_modules/**',
-            ],
-            delimiters: ['', ''],
-            values: {
-                'process.env.GIT_REVISION': JSON.stringify(GIT_REVISION),
-                'process.env.BUILD_DATE': JSON.stringify(BUILD_DATE),
-            },
-        }),
-        replace({
-            include: [
-                '**/redux/es/redux.js',
-            ],
-            delimiters: ['', ''],
-            values: {
-                'process.env.NODE_ENV': JSON.stringify(LIB_NODE_ENV),
-            },
-        }),
+            // https://github.com/rollup/rollup-plugin-replace
+            replace({
+                exclude: [
+                    'node_modules/**',
+                ],
+                delimiters: ['', ''],
+                values: {
+                    'process.env.GIT_REVISION': JSON.stringify(GIT_REVISION),
+                    'process.env.BUILD_DATE': JSON.stringify(BUILD_DATE),
+                },
+            }),
+            replace({
+                include: [
+                    '**/redux/es/redux.js',
+                ],
+                delimiters: ['', ''],
+                values: {
+                    'process.env.NODE_ENV': JSON.stringify(LIB_NODE_ENV),
+                },
+            }),
 
-        // https://github.com/rollup/rollup-plugin-babel
-        babel({
-            exclude: 'node_modules/**',
-            externalHelpers: false,
-            babelrc: false,
-            presets: [
-                ['@babel/preset-react', {
-                    // https://github.com/babel/babel/tree/master/packages/babel-preset-react#options
-                    development: !IS_PRODUCTION_MODE,
-                }],
-            ],
-            plugins: [
-                '@babel/plugin-syntax-dynamic-import',
-                '@babel/plugin-syntax-async-generators',
-            ],
-        }),
-    ],
+            // https://github.com/rollup/rollup-plugin-babel
+            babel({
+                exclude: 'node_modules/**',
+                externalHelpers: false,
+                babelrc: false,
+                presets: [
+                    ['@babel/preset-react', {
+                        // https://github.com/babel/babel/tree/master/packages/babel-preset-react#options
+                        development: !IS_PRODUCTION_MODE,
+                    }],
+                ],
+                plugins: [
+                    '@babel/plugin-syntax-dynamic-import',
+                    '@babel/plugin-syntax-async-generators',
+                ],
+            }),
+        ],
+    };
 };
