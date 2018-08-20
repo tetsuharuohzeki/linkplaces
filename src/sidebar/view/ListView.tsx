b1b53475f614ab6bd1dc78a7ecbd6193f9695fbd
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-08-21 03:19:47 +0900

    Use <PanelSectionList> for the impl of <ListItemView/>

--- src/sidebar/view/ListView.tsx
+++ src/sidebar/view/ListView.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { PanelSectionList } from '../../shared/component/PanelSectionList';
+
 export interface ListItemViewProps {
     children: React.ReactNode;
 }
 
 export function ListItemView(props: ListItemViewProps): JSX.Element {
     return (
-        <div>
-            <ul className={'sidebar__ListView_container'}>
-                {props.children}
-            </ul>
-        </div>
+        <PanelSectionList>
+            {props.children}
+        </PanelSectionList>
     );
 }
 (ListItemView as React.StatelessComponent<ListItemViewProps>).propTypes = {
