17276b85a7b3cc6a76c793c5041bfc1dd314b8a9
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-08-22 23:57:38 +0900

    Add ViewContext

--- src/shared/ViewContext.ts
+++ src/shared/ViewContext.ts
@@ -5,3 +5,18 @@ export interface SyncViewContext {
     onResume(mountpoint: Element): void;
     onSuspend(mountpoint: Element): void;
 }
+
+/**
+ *  Some operations requires to call it in the async context
+ *  and we need to handle it correctly as async operation.
+ *  In JavaScript which does not have any thread and we cannot block
+ *  main thread, I seem it's compromise approach to return
+ *  return `Promise` from lyfecycle methods.
+ */
+export interface ViewContext extends SyncViewContext {
+    onActivate(mountpoint: Element): Promise<void>;
+    onDestroy(mountpoint: Element): Promise<void>;
+
+    onResume(mountpoint: Element): Promise<void>;
+    onSuspend(mountpoint: Element): Promise<void>;
+}
