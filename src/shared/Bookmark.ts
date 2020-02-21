e7604b3e5fd8ad84fd0d04f027a4df30f846e15d
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2020-02-21 14:39:31 +0900

    Apply importsNotUsedAsValues=error for shared/

--- src/shared/Bookmark.ts
+++ src/shared/Bookmark.ts
@@ -2,7 +2,7 @@ import { isNull } from 'option-t/esm/Nullable/Nullable';
 import { Result, createErr, createOk } from 'option-t/esm/PlainResult/Result';
 import { unwrapFromResult } from 'option-t/esm/PlainResult/unwrap';
 
-import {
+import type {
     BookmarkTreeNode,
     BookmarkTreeNodeItem,
     BookmarkTreeNodeFolder,
--- src/shared/Store.ts
+++ src/shared/Store.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import type { Observable } from 'rxjs';
 
 export interface Store<T> {
     compose(initial: Readonly<T>): Observable<T>;
