40e1867916baaff5f9464345e1b71b922fd6cea4
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-02-18 05:10:45 +0900

    Revert "Enable noPropertyAccessFromIndexSignature tsc option"
    This reverts commit c5d2f82397ebbe6183d7ce7659e828eee9c657ad.

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,13 +1,12 @@
 /* global process:readable */
 
 // this will be embedded by the linker.
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion, dot-notation
-export const GIT_REVISION: string = process.env['GIT_REVISION']!;
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export const GIT_REVISION: string = process.env.GIT_REVISION!;
 
 // this will be embedded by the linker.
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion, dot-notation
-export const BUILD_DATE: string = process.env['BUILD_DATE']!;
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export const BUILD_DATE: string = process.env.BUILD_DATE!;
 
 // this will be embedded by the linker.
-// eslint-disable-next-line dot-notation
-export const USE_REACT_CONCURRENT_MODE: boolean = !!process.env['USE_REACT_CONCURRENT_MODE'];
+export const USE_REACT_CONCURRENT_MODE: boolean = !!process.env.USE_REACT_CONCURRENT_MODE;
