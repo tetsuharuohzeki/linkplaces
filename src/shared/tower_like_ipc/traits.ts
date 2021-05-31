e88eafb4fed68b7633a1456927d5b2dfa2156f05
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-05-31 23:09:45 +0900

    Add comment about https://tokio.rs/blog/2021-05-14-inventing-the-service-trait

--- src/shared/tower_like_ipc/traits.ts
+++ src/shared/tower_like_ipc/traits.ts
@@ -1,5 +1,6 @@
-import type { Result } from 'option-t/esm/PlainResult';
+// Inspired by https://tokio.rs/blog/2021-05-14-inventing-the-service-trait
 
+import type { Result } from 'option-t/esm/PlainResult';
 export interface TowerService<TRequest, TResponse> {
     ready(): Promise<Result<void, Error>>;
     call(req: TRequest): Promise<TResponse>;
