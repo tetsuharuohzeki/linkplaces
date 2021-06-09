ddbeda13998c77093b34f7a35de97613bb9f6e44
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-06-10 00:56:32 +0900

    Fix redundant types of toArray of Ix

--- src/shared/ix/to_array.ts
+++ src/shared/ix/to_array.ts
@@ -1,6 +1,4 @@
-type Consumable<T> = Iterable<T> | Generator<T>;
-
-export function toArrayFromIterable<T>(iter: Consumable<T>): Array<T> {
+export function toArrayFromIterable<T>(iter: Iterable<T>): Array<T> {
     const result: Array<T> = [];
     for (const item of iter) {
         result.push(item);
--- src/shared/ix/to_array_async.ts
+++ src/shared/ix/to_array_async.ts
@@ -1,6 +1,4 @@
-type AsyncConsumable<T> = Iterable<T> | Generator<T>;
-
-export async function toArrayAsyncFromAsyncIterable<T>(iter: AsyncConsumable<T>): Promise<Array<T>> {
+export async function toArrayAsyncFromAsyncIterable<T>(iter: AsyncIterable<T>): Promise<Array<T>> {
     const result: Array<T> = [];
     for await (const item of iter) {
         result.push(item);
