5dcb2117acbdaa22436e06b28ef5a415176b4b16
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-07-10 04:29:51 +0900

    Set iterable to SidebarState.list

--- src/sidebar/SidebarState.ts
+++ src/sidebar/SidebarState.ts
@@ -1,13 +1,16 @@
 import type { BookmarkTreeNode } from '../../typings/webext/bookmarks';
+
+import * as Ix from '../foundation/ix/mod';
 import { mapToSidebarItemEntity, SidebarItemViewModelEntity } from './SidebarDomain';
 
 export interface SidebarState {
     list: Iterable<SidebarItemViewModelEntity>;
 }
 
-export function createInitialSidebarState(list: Array<BookmarkTreeNode> = []): SidebarState {
+export function createInitialSidebarState(source: Array<BookmarkTreeNode> = []): SidebarState {
+    const list = Ix.map(source, mapToSidebarItemEntity);
     const initialState: Readonly<SidebarState> = {
-        list: list.map(mapToSidebarItemEntity),
+        list,
     };
     return initialState;
 }
