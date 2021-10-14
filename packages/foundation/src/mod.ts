5f9a7bc3a2cebbb6f0169dde8a56a355f00273b6
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-10-15 02:51:37 +0900

    Move NoImplementationError to foundation

--- packages/foundation/src/mod.ts
+++ packages/foundation/src/mod.ts
@@ -2,3 +2,4 @@ export * as Ix from './ix/mod.js';
 export * as Ipc from './tower_like_ipc/mod.js';
 export * from './view_ctx/mod.js';
 export * from './domfactory.js';
+export { NoImplementationError } from './NoImplementationError.js';
