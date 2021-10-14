69a5475329cb45e0ae4eca988fd6406b3277e120
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-10-15 04:23:58 +0900

    Move background code to background_service

---
+++ packages/background_service/src/mod.ts
@@ -0,0 +1,26 @@
+import { Ipc } from '@linkplaces/foundation';
+import { assertIsRemoteAction } from '@linkplaces/ipc_message';
+
+import { BackgroundRemoteActionReciever } from './BackgroundRemoteActionReciever';
+import { createContextMenu } from './ContextMenu';
+declare global {
+    // We keep this for debugging.
+    // eslint-disable-next-line no-var
+    var livingConnectionSet: WeakSet<Ipc.ServerConnection<unknown, unknown>>;
+}
+
+(function main() {
+    createContextMenu();
+
+    globalThis.livingConnectionSet = new WeakSet();
+
+    browser.runtime.onConnect.addListener((portToSender) => {
+        const service = new BackgroundRemoteActionReciever();
+        const wrapper = new Ipc.OneShotResponder(assertIsRemoteAction, service);
+        const server = new Ipc.ServerConnection(portToSender, wrapper);
+
+        globalThis.livingConnectionSet.add(server);
+
+        server.run();
+    });
+})();
