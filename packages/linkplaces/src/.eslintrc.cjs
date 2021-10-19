de9922b269e469da0fd2c6a5c4048e43fcf5f792
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2021-10-20 01:50:00 +0900

    Move configs related to typescript-eslint to `overrides` section

--- packages/linkplaces/src/.eslintrc.cjs
+++ packages/linkplaces/src/.eslintrc.cjs
@@ -1,6 +1,5 @@
 /*eslint-env node*/
 /*eslint quote-props: [2, "always"] */
-/* eslint-disable @typescript-eslint/no-require-imports */
 
 // eslint-disable-next-line strict
 'use strict';
@@ -16,29 +15,18 @@ const ALL_EXTENSION = [...TS_EXTENSION, ...JS_EXTENSION];
 module.exports = {
     'extends': [
         'eslint-config-abema/config/eslintrc_react_hooks.js',
-        'eslint-config-abema/config/eslintrc_typescript.js',
-        'eslint-config-abema/config/eslintrc_typescript_react.js',
     ],
 
     'parserOptions': {
-        'ecmaVersion': 2021,
         'sourceType': 'module',
         'ecmaFeatures': {
             'jsx': true
         },
-        'project': path.resolve(__dirname, '../tsconfig.eslint.json'),
-        'extraFileExtensions': ['.cjs'],
     },
 
     'env': {
         'browser': true,
         'webextensions': true,
-    },
-
-    'globals': {
-        // see https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        'JSX': 'readonly',
     },
 
     'settings': {
@@ -49,8 +37,33 @@ module.exports = {
         },
     },
 
-    'rules': {
-        // FIXME: Re-enable for the future.
-        '@typescript-eslint/member-ordering': 'off',
-    },
+    'overrides': [
+        {
+            'files': ['*.ts', '*.tsx', '*.d.ts'],
+
+            'extends': [
+                'eslint-config-abema/config/eslintrc_typescript.js',
+                'eslint-config-abema/config/eslintrc_typescript_react.js',
+            ],
+
+            'parserOptions': {
+                'sourceType': 'module',
+                'ecmaFeatures': {
+                    'jsx': true
+                },
+                'project': path.resolve(__dirname, '../tsconfig.eslint.json'),
+                'extraFileExtensions': ['.cjs'],
+            },
+
+            'globals': {
+                // see https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+                'JSX': 'readonly',
+            },
+
+            'rules': {
+                // FIXME: Re-enable for the future.
+                '@typescript-eslint/member-ordering': 'off',
+            },
+        },
+    ],
 };
