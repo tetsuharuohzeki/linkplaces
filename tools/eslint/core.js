bb0d223e8a0bf81e245309db9a0c010b78939b42
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-10-03 19:42:27 +0900

    ESLint: Reorganize the construction for `projectRules`

--- tools/eslint/core.js
+++ tools/eslint/core.js
@@ -4,10 +4,35 @@ import { config as prettierRules } from './prettier.js';
 import corePresets from './vendor/core.cjs';
 import importPresets from './vendor/import.cjs';
 
-const projectRules = Object.freeze({
+const builtinRules = Object.freeze({
+    // In JavaScript, after ES Module or CommonJS era (single module per single file),
+    // excluding the case to improve an API ergonomics or requirement to implement an object interface,
+    // we don't have to belong a function that does not touch `this` to a class unlike Java or C++.
+    // We should not do such a thing.
+    //
+    // This rule bans a class instance method
+    // that does not touch any `this` to improve a possibility to minify a code.
+    //
+    // Additionally, it's hard to minify a property that is on prototype chain.
+    // Typically, they appears as a pattern as class' instance method.
+    // We cannot remove or mangle a code like `a.foo()` style code
+    // without analysis for whole of programs including usages of reflection
+    // or identifying what item is a part of public interface.
+    //
+    // To get a chance to improve a code size performance and to simplify a code,
+    // it's better that we should export a standalone function directly
+    // instead of a part of class if it does not affect an API ergonomics.
+    'class-methods-use-this': 'warn',
+
+    // This prevents the pattern that is error prone.
     'no-constant-binary-expression': 'error',
-    'no-magic-numbers': 'off',
+
+    // This detects unused field easily.
     'no-unused-private-class-members': 'warn',
+});
+
+const projectRules = Object.freeze({
+    'no-magic-numbers': 'off',
 
     'no-restricted-imports': [
         'error',
@@ -52,29 +77,11 @@ export const config = Object.freeze({
     },
     rules: {
         ...corePresets.rules,
+        ...builtinRules,
         ...importPresets.rules,
         ...prettierRules.rules,
         ...projectRules,
         ...rulesForESModule,
-
-        // In JavaScript, after ES Module or CommonJS era (single module per single file),
-        // excluding the case to improve an API ergonomics or requirement to implement an object interface,
-        // we don't have to belong a function that does not touch `this` to a class unlike Java or C++.
-        // We should not do such a thing.
-        //
-        // This rule bans a class instance method
-        // that does not touch any `this` to improve a possibility to minify a code.
-        //
-        // Additionally, it's hard to minify a property that is on prototype chain.
-        // Typically, they appears as a pattern as class' instance method.
-        // We cannot remove or mangle a code like `a.foo()` style code
-        // without analysis for whole of programs including usages of reflection
-        // or identifying what item is a part of public interface.
-        //
-        // To get a chance to improve a code size performance and to simplify a code,
-        // it's better that we should export a standalone function directly
-        // instead of a part of class if it does not affect an API ergonomics.
-        'class-methods-use-this': 'warn',
     },
     settings: {
         ...importPresets.settings,
