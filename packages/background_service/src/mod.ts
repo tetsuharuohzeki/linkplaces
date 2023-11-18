4961c1c657b965ee696d2a90980cf9c36c333faf
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-11-18 23:40:13 +0900

    Remove `any` type usage from packages/background_service/src/mod.ts

--- packages/background_service/src/mod.ts
+++ packages/background_service/src/mod.ts
@@ -1,13 +1,13 @@
 import { SendMessageResponder } from '@linkplaces/foundation/tower_like_ipc';
-import { assertIsRemoteAction } from '@linkplaces/ipc_message';
+import { assertIsRemoteAction, type RemoteAction } from '@linkplaces/ipc_message';
 
 import { BackgroundRemoteActionReciever } from './BackgroundRemoteActionReciever.js';
 import { setupContextMenus } from './ContextMenu.js';
 
 declare global {
     // We keep this for debugging.
-    // eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
-    var livingConnectionSet: WeakSet<SendMessageResponder<any, any>>;
+    // eslint-disable-next-line no-var
+    var livingConnectionSet: WeakSet<SendMessageResponder<RemoteAction, void>>;
 }
 
 (function main() {
