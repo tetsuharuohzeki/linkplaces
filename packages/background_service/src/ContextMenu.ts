8004735f02e5d6fd50a0de62c35af1dfd1f23fbb
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-11-22 00:37:35 +0900

    Move the context menu initialization to the top level

--- packages/background_service/src/ContextMenu.ts
+++ packages/background_service/src/ContextMenu.ts
@@ -23,18 +23,7 @@ function createCtxMenuArg(id: string, title: string, contexts: Array<ContextType
     };
 }
 
-export function setupContextMenus(browser: typeof chrome): void {
-    browser.menus.onClicked.addListener(onClicked);
-
-    // [By the backgroun page doc on MDN](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Background_scripts),
-    // an extension should use `browser.runtime.onInstalled` to register a menu
-    // But this code only works once after user install extension
-    // on [Firefox 106.0a1](https://hg.mozilla.org/mozilla-central/rev/c731914e80964349114e473544e7a7165cae3cc1)
-    // So we need place this intializer in the top level scope even if the background page is non-persistent.
-    createContextMenu(browser);
-}
-
-function createContextMenu(browser: typeof chrome): void {
+export function appendContextMenu(browser: typeof chrome): void {
     const list: Array<CreateArgument> = [
         createCtxMenuArg(CTXMENU_ID_TAB_SAVE_TAB, 'Add Tab to LinkPlaces', ['tab']),
         createCtxMenuArg(CTXMENU_ID_CONTENT_SAVE_PAGE, 'Add Page to LinkPlaces', ['page']),
@@ -61,11 +50,11 @@ function createContextMenu(browser: typeof chrome): void {
 }
 
 export function removeContextMenu(browser: typeof chrome): Promise<void> {
-    browser.menus.onClicked.removeListener(onClicked);
+    browser.menus.onClicked.removeListener(onClickContextMenu);
     return browser.menus.removeAll();
 }
 
-function onClicked(info: OnClickData, tab: Maybe<Tab>): void {
+export function onClickContextMenu(info: OnClickData, tab: Maybe<Tab>): void {
     onClickedAsync(info, tab).catch(console.error);
 }
 
--- packages/background_service/src/mod.ts
+++ packages/background_service/src/mod.ts
@@ -2,12 +2,19 @@ import { callResponderServiceWithMessage } from '@linkplaces/foundation/tower_li
 import { assertIsRemoteAction } from '@linkplaces/ipc_message';
 
 import { callBackgroundRemoteActionReciever } from './BackgroundRemoteActionReciever.js';
-import { setupContextMenus } from './ContextMenu.js';
+import { appendContextMenu, onClickContextMenu } from './ContextMenu.js';
 
 (function main() {
-    setupContextMenus(browser);
-
     const runtime = browser.runtime;
+    const menus = browser.menus;
+
+    // [By the backgroun page doc on MDN](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Background_scripts),
+    // an extension should use `browser.runtime.onInstalled` to register a menu
+    // But this code only works once after user install extension
+    // on [Firefox 106.0a1](https://hg.mozilla.org/mozilla-central/rev/c731914e80964349114e473544e7a7165cae3cc1)
+    // So we need place this intializer in the top level scope even if the background page is non-persistent.
+    appendContextMenu(browser);
+    menus.onClicked.addListener(onClickContextMenu);
 
     runtime.onMessage.addListener(async function onMessage(message, sender) {
         const res = await callResponderServiceWithMessage(
