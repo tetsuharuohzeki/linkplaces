ac9d7e3bcd400bbc7801a785546f904f34de3eaa
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-02-08 04:52:01 +0900

    rx: Implement fromAsyncIterableToObservable

--- packages/foundation/src/rx/mod.ts
+++ packages/foundation/src/rx/mod.ts
@@ -1,5 +1,6 @@
 export { type SyncFactoryFn, createObservable } from './observables/create.js';
 export { type AsyncFactoryFn, createObservableFromAsync } from './observables/create_async.js';
+export { fromAsyncIterableToObservable } from './observables/from_async_iterable.js';
 export { fromEventToObservable } from './observables/from_event.js';
 
 export * as operators from './operators/mod.js';
---
+++ packages/foundation/src/rx/observables/from_async_iterable.ts
@@ -0,0 +1,21 @@
+import { Observable } from '../observable.js';
+import { createObservableFromAsync } from './create_async.js';
+import type { Observer } from '../observer.js';
+
+async function iterate<T>(factory: AsyncIterable<T>, observer: Observer<T>, signal: AbortSignal): Promise<void> {
+    for await (const item of factory) {
+        if (signal.aborted) {
+            break;
+        }
+
+        observer.next(item);
+    }
+}
+
+export function fromAsyncIterableToObservable<T>(factory: AsyncIterable<T>): Observable<T> {
+    const o = createObservableFromAsync<T>(async (observer, signal) => {
+        const promise = iterate<T>(factory, observer, signal);
+        return promise;
+    });
+    return o;
+}
