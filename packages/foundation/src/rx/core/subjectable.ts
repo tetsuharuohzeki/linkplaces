8e2d8d9c112a59b782422b0800b07a4bb0aab729
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-02-29 00:49:44 +0900

    rx: `Subjectable` should be the derived interface of `Observer`

--- packages/foundation/src/rx/core/subjectable.ts
+++ packages/foundation/src/rx/core/subjectable.ts
@@ -1,6 +1,7 @@
+import type { Observer } from '../mod';
 import type { Unsubscribable } from './subscribable';
 import type { Subscriber } from './subscriber';
 
-export interface Subjectable<T> extends Subscriber<T> {
+export interface Subjectable<T> extends Observer<T> {
     subscribe(destination: Subscriber<T>): Unsubscribable;
 }
