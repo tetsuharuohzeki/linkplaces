a18d67e536f2ca643bb65eb62f552ed160616bc2
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-03-07 03:30:56 +0900

    Move Ix impls into subdir

--- packages/foundation/src/ix/mod.ts
+++ packages/foundation/src/ix/mod.ts
@@ -1,16 +1,17 @@
-export { filterForIterable as filter } from './filter.js';
-export { filterAsyncForAsyncIterable as filterAsync } from './filter_async.js';
+export { filterForIterable as filter } from './impls/filter.js';
+export { filterAsyncForAsyncIterable as filterAsync } from './impls/filter_async.js';
 export {
     filterMapForIterable as filterMap,
-    filterMapWithComparatorForIterable as filterMapWithComparator,
-} from './filter_map.js';
+    filterMapWithComparatorForIterable as filterMapWithComparator
+} from './impls/filter_map.js';
 export {
     filterMapAsyncForAsyncIterable as filterMapAsync,
-    filterMapAsyncWithComparatorForAsyncIterable as filterMapAsyncWithComparator,
-} from './filter_map_async.js';
-export { flatMapForIterable as flatMap } from './flat_map.js';
-export { mapForIterable as map } from './map.js';
-export { mapAsyncForAsyncIterable as mapAsync } from './map_async.js';
-export { toArrayFromIterable as toArray } from './to_array.js';
-export { toArrayAsyncFromAsyncIterable as toArrayAsync } from './to_array_async.js';
-export { zipForIterable as zip } from './zip.js';
+    filterMapAsyncWithComparatorForAsyncIterable as filterMapAsyncWithComparator
+} from './impls/filter_map_async.js';
+export { flatMapForIterable as flatMap } from './impls/flat_map.js';
+export { mapForIterable as map } from './impls/map.js';
+export { mapAsyncForAsyncIterable as mapAsync } from './impls/map_async.js';
+export { toArrayFromIterable as toArray } from './impls/to_array.js';
+export { toArrayAsyncFromAsyncIterable as toArrayAsync } from './impls/to_array_async.js';
+export { zipForIterable as zip } from './impls/zip.js';
+
