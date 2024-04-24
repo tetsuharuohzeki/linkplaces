6de2aea6f4652e5602928ae6644faf6e653645ad
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-04-25 04:35:42 +0900

    Replace picomatch based one with Node.js' builtin fs.glob

--- packages/linkplaces/tools/glob.js
+++ packages/linkplaces/tools/glob.js
@@ -1,40 +1,12 @@
 import * as fs from 'node:fs/promises';
-import * as path from 'node:path';
-
-import picomatch from 'picomatch';
-
-async function* getAllEntriesInDirectory(rootDir) {
-    const files = await fs.readdir(rootDir, {
-        withFileTypes: true,
-    });
-
-    for (const file of files) {
-        const filename = file.name;
-        const fullFilepath = path.join(rootDir, filename);
-
-        if (file.isFile()) {
-            yield fullFilepath;
-        }
-        else if (file.isDirectory()) {
-            yield fullFilepath;
-            yield* getAllEntriesInDirectory(fullFilepath);
-        } else {
-            // Othe type is not supported.
-            continue;
-        }
-    }
-}
 
 export async function getAllGlobMatchedFiles(rootDir, globPattern) {
-    const isMatch = picomatch(globPattern);
     const result = [];
-    for await (const file of getAllEntriesInDirectory(rootDir)) {
-        if (!isMatch(file)) {
-            continue;
-        }
-
+    const enumerator = fs.glob(globPattern, {
+        cwd: rootDir,
+    });
+    for await (const file of enumerator) {
         result.push(file);
     }
-
     return result;
 }
