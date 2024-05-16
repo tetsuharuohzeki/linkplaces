16ba6e02304a42eaea026467dff3824361d3122d
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-05-16 20:00:19 +0900

    babel phase for react compiler should do react's optimization if ENABLE_SWC_REACT_TRANSFORM is true

--- packages/linkplaces/tools/babel_config.mjs
+++ packages/linkplaces/tools/babel_config.mjs
@@ -1,4 +1,4 @@
-import { IS_PRODUCTION_MODE, ENABLE_REACT_COMPILER } from './buildconfig.js';
+import { IS_PRODUCTION_MODE, ENABLE_REACT_COMPILER, ENABLE_SWC_REACT_TRANSFORM } from './buildconfig.js';
 
 const REACT_COMPILER_CONFIG = {};
 
@@ -6,27 +6,41 @@ const plugins = ENABLE_REACT_COMPILER
     ? [
           // @prettier-ignore
           ['babel-plugin-react-compiler', REACT_COMPILER_CONFIG],
+          // This is required to parse jsx syntax.
+          // react compiler is required to place to the first.
+          // So other transformer have not transformed jsx yet.
+          ['@babel/plugin-syntax-jsx'],
       ]
     : [];
 
-const presets = [
-    [
-        '@babel/preset-react',
-        {
-            // https://github.com/babel/babel/tree/master/packages/babel-preset-react#options
-            development: !IS_PRODUCTION_MODE,
-            useSpread: true,
-            runtime: 'automatic',
-        },
-    ],
-];
-
 export const cliConfig = {
-    presets,
+    presets: [
+        [
+            '@babel/preset-react',
+            {
+                // https://github.com/babel/babel/tree/master/packages/babel-preset-react#options
+                development: !IS_PRODUCTION_MODE,
+                useSpread: true,
+                runtime: 'automatic',
+            },
+        ],
+    ],
     plugins,
 };
 
 export const rollupConfig = {
-    presets,
+    presets: ENABLE_SWC_REACT_TRANSFORM
+        ? []
+        : [
+              [
+                  '@babel/preset-react',
+                  {
+                      // https://github.com/babel/babel/tree/master/packages/babel-preset-react#options
+                      development: !IS_PRODUCTION_MODE,
+                      useSpread: true,
+                      runtime: 'automatic',
+                  },
+              ],
+          ],
     plugins,
 };
