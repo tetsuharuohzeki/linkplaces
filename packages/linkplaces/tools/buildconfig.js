bae8970618a86509d3602897cabe5af964b5ae2e
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-05-31 02:01:57 +0900

    Revert "Allow to configure `ENABLE_REACT_COMPILER` only in the production build"
    This reverts commit 0d5bea00c217bfb16140ac7f6f3ca56421b11059.
    This does not works correctly

--- packages/linkplaces/tools/buildconfig.js
+++ packages/linkplaces/tools/buildconfig.js
@@ -1,34 +1,19 @@
 import * as assert from 'node:assert/strict';
 
-import { mapOrForMaybe, mapForMaybe } from 'option-t/maybe';
+import { mapOrForMaybe } from 'option-t/maybe';
 
 const GIT_REVISION = mapOrForMaybe(process.env.GIT_REVISION, 'unknown', String);
 const BUILD_DATE = mapOrForMaybe(process.env.BUILD_DATE, 'unknown', String);
 
-const RELEASE_CHANNEL_VALUE_PRODUCTION = 'production';
-const RELEASE_CHANNEL_VALUE_DEVELOPMENT = 'development';
-
 const RELEASE_CHANNEL = mapOrForMaybe(process.env.RELEASE_CHANNEL, 'production', String);
-const LIB_NODE_ENV =
-    RELEASE_CHANNEL === 'production' ? RELEASE_CHANNEL_VALUE_PRODUCTION : RELEASE_CHANNEL_VALUE_DEVELOPMENT;
-
-const IS_PRODUCTION_MODE = RELEASE_CHANNEL === RELEASE_CHANNEL_VALUE_PRODUCTION;
-const IS_DEVELOPMENT_MODE = RELEASE_CHANNEL === RELEASE_CHANNEL_VALUE_DEVELOPMENT;
+const LIB_NODE_ENV = RELEASE_CHANNEL === 'production' ? 'production' : 'development';
+const IS_PRODUCTION_MODE = RELEASE_CHANNEL === 'production';
 
 const SHOULD_FORCE_ENABLE_SOURCE_MAP = process.env.FORCE_ENABLE_SOURCE_MAP === 'true';
 const ENABLE_SOURCE_MAP = SHOULD_FORCE_ENABLE_SOURCE_MAP || !IS_PRODUCTION_MODE;
 
 const ENABLE_SWC_REACT_TRANSFORM = process.env.ENABLE_SWC_REACT_TRANSFORM !== 'false';
-const ENABLE_REACT_COMPILER = mapForMaybe(process.env.ENABLE_REACT_PROFILER, (val) => {
-    if (IS_DEVELOPMENT_MODE) {
-        // make debugging easier
-        return false;
-    }
-
-    assert.ok(IS_PRODUCTION_MODE, `ENABLE_REACT_COMPILER is configurable only in the production build`);
-    const enabled = val === 'false';
-    return enabled;
-});
+const ENABLE_REACT_COMPILER = process.env.ENABLE_REACT_COMPILER !== 'false';
 
 const ENABLE_REACT_PROFILER = mapOrForMaybe(process.env.ENABLE_REACT_PROFILER, false, (val) => {
     assert.ok(IS_PRODUCTION_MODE, `ENABLE_REACT_PROFILER is only enabled in the production build`);
