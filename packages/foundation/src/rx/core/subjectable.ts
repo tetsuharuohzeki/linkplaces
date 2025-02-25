7e82a39700f3dea597ba3038b569bd3ba942d3b9
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-02-25 20:00:29 +0900

    Add `Subjectable.asObservable()` constraint

--- packages/foundation/src/rx/core/subjectable.ts
+++ packages/foundation/src/rx/core/subjectable.ts
@@ -1,4 +1,6 @@
 import type { ObservableLike } from './observable';
 import type { Observer } from './observer';
 
-export interface Subjectable<T> extends Observer<T>, ObservableLike<T> {}
+export interface Subjectable<T> extends Observer<T> {
+    asObservable(): ObservableLike<T>;
+}
