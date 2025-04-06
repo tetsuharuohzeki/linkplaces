f45b3ff131eb51886c07ffb55caa35c27aa02744
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-04-06 16:03:08 +0900

    Cleanup the rule of `@typescript-eslint/naming-convention`

--- packages/eslint_config/src/configs/typescript/naming-convention.js
+++ packages/eslint_config/src/configs/typescript/naming-convention.js
@@ -1,3 +1,8 @@
+const detaultLeadingUnderscorePolicy = {
+    // Basically, unused items should be removed.
+    leadingUnderscore: 'forbid',
+};
+
 /**
  *  @type   {import("eslint").Linter.RulesRecord}
  *  @see    https://typescript-eslint.io/rules/naming-convention/
@@ -7,12 +12,19 @@ export const rules = {
     camelcase: 'off',
     '@typescript-eslint/naming-convention': [
         'warn',
+
+        // Rule definition order is not used to applying a rule selector.
+        // https://typescript-eslint.io/rules/naming-convention/#how-does-the-rule-automatically-order-selectors
+        // Thus, we need opt-out to them..
         {
             selector: 'default',
             format: ['camelCase'],
             leadingUnderscore: 'allow',
+            // allow to similar semantic items
+            trailingUnderscore: 'allow',
         },
 
+        // MARK: Detailed configs covered by `variableLike`
         {
             selector: 'variable',
             format: [
@@ -21,60 +33,34 @@ export const rules = {
                 // We need allow `PascalCase` for React component.
                 // e.g.
                 //  ```
-                //  const SomeContext = React.useContext();
-                //
+                //  const BarContext = React.useContext();
                 //  const MemoizedComponent = React.memo(function SomeComponent() {});
+                //  const
                 //  ```
                 'PascalCase',
             ],
+            ...detaultLeadingUnderscorePolicy,
         },
-
         {
             selector: 'function',
             // For React's Function Component, we need allow `PascalCase`.
             format: ['camelCase', 'PascalCase'],
+            ...detaultLeadingUnderscorePolicy,
         },
 
-        {
-            selector: 'parameter',
-            format: ['camelCase'],
-            leadingUnderscore: 'allow',
-        },
-
-        // Enforce that private members are prefixed with an underscore
-        // By these reasons, I think we recommend to add `_` prefix to private fields.
-        //
-        //  * Historically, JavaScript wolrd use `_` prefix to mark fields as _private_.
-        //  * Until coming [private fields of class field declarations proposal](https://github.com/tc39/proposal-class-fields),
-        //    there is no true private fields in JavaScript.
-        //      * If TypeScript compiler supports it, it might be better to relax this rule.
-        //  * TypeScript will be transformed into plain JavaScript and plain JavaScript does not any informations
-        //    to express whether a field is private or not.
-        {
-            selector: 'memberLike',
-            modifiers: ['private', 'protected'],
-            format: ['camelCase'],
-            // FIXME: Does this option really work?
-            // See #378
-            leadingUnderscore: 'require',
-        },
+        // MARK: Detailed configs covered by `memberLike`
         {
             selector: 'enumMember',
             format: ['PascalCase'],
+            ...detaultLeadingUnderscorePolicy,
         },
 
+        // MARK: Detailed configs covered by `typeLike`
         {
             selector: 'typeLike',
             format: ['PascalCase'],
+            ...detaultLeadingUnderscorePolicy,
         },
-        // A class & interface should be PascalCased
-        {
-            selector: 'class',
-            // Don't export to make it private.
-            leadingUnderscore: 'forbid',
-            format: ['PascalCase'],
-        },
-
         // Enforce that interface names do not begin with an I
         // [By TypeScript coding guidelines](https://github.com/Microsoft/TypeScript/wiki/Coding-guidelines),
         // This rule is banned.
@@ -83,14 +69,12 @@ export const rules = {
         {
             selector: 'interface',
             format: ['PascalCase'],
-            // Don't export to make it private.
-            leadingUnderscore: 'forbid',
             custom: {
                 regex: '^I[A-Z]',
                 match: false,
             },
+            ...detaultLeadingUnderscorePolicy,
         },
-
         //  * We accept the style for T , TA , TAbc , TA1Bca , T1 , T2.
         //      * You seem this style is similar to C# or typescript compiler.
         //      * This choise is for:
@@ -109,11 +93,31 @@ export const rules = {
             //    'regex': '^T([A-Z0-9][a-zA-Z0-9]*){0,1}$',
             //    'match': true,
             //},
+            ...detaultLeadingUnderscorePolicy,
         },
 
+        // MARK: not covered by group selectors
         {
             selector: 'import',
             format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+            ...detaultLeadingUnderscorePolicy,
+        },
+
+        {
+            //
+            // Enforce that private members are prefixed with an underscore
+            // By these reasons, I think we recommend to add `_` prefix to private fields.
+            //
+            //  * Historically, JavaScript wolrd use `_` prefix to mark fields as _private_.
+            //  * Until coming [private fields of class field declarations proposal](https://github.com/tc39/proposal-class-fields),
+            //    there is no true private fields in JavaScript.
+            //      * If TypeScript compiler supports it, it might be better to relax this rule.
+            //  * TypeScript will be transformed into plain JavaScript and plain JavaScript does not any informations
+            //    to express whether a field is private or not.
+            selector: ['classProperty', 'classMethod', 'autoAccessor', 'classicAccessor'],
+            modifiers: ['private', 'protected'],
+            format: ['camelCase'],
+            ...detaultLeadingUnderscorePolicy,
         },
     ],
 };
