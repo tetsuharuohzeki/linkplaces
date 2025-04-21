ee16f14b0a71dbca387a42a51cf718bd74495651
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-04-22 03:27:53 +0900

    Enable `@eslint/css`

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import css from '@eslint/css';
 import js from '@eslint/js';
 import {
     coreConfig,
@@ -19,14 +20,24 @@ import turboConfig from 'eslint-config-turbo/flat';
 const THIS_FILE_NAME = fileURLToPath(import.meta.url);
 const THIS_DIR_NAME = path.dirname(THIS_FILE_NAME);
 
+const FILE_PATTERN_CSS = ['**/*.css'];
 const FILE_PATTERN_JS_ESM = ['**/*.mjs', '**/*.js'];
 const FILE_PATTERN_JS_CJS = ['**/*.cjs'];
 const FILE_PATTERN_TS = ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'];
+const FILE_PATTERNS_ALL_JS_AND_TS = Array.from(
+    new Set([...FILE_PATTERN_JS_ESM, ...FILE_PATTERN_JS_CJS, ...FILE_PATTERN_TS])
+);
 
 // eslint-disable-next-line import/no-default-export -- ESLint requires default export.
 export default defineConfig([
-    js.configs.recommended,
-    coreConfig,
+    {
+        files: FILE_PATTERNS_ALL_JS_AND_TS,
+        ...js.configs.recommended,
+    },
+    {
+        files: FILE_PATTERNS_ALL_JS_AND_TS,
+        ...coreConfig,
+    },
     {
         linterOptions: {
             reportUnusedDisableDirectives: 'warn',
@@ -92,6 +103,22 @@ export default defineConfig([
     {
         files: ['**/__tests__/*'],
         languageOptions: languageOptionsForTesting,
+    },
+    {
+        files: FILE_PATTERN_CSS,
+        language: 'css/css',
+        ...css.configs.recommended,
+    },
+    {
+        files: FILE_PATTERN_CSS,
+        plugins: {
+            css,
+        },
+        language: 'css/css',
+        rules: {
+            'css/use-baseline': 'off', // This project only works for Firefox.
+            'css/prefer-logical-properties': 'warn',
+        },
     },
     ...turboConfig,
     ...prettierConfigs,
