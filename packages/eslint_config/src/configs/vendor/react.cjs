7f5429a100c07b403a9276f6257ebddd330c806b
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-07-13 02:25:36 +0900

    eslint: Remove disabled rules for eslint-plugin-react

--- packages/eslint_config/src/configs/vendor/react.cjs
+++ packages/eslint_config/src/configs/vendor/react.cjs
@@ -8,36 +8,11 @@ module.exports = {
     // ESLint-plugin-React
     // https://github.com/yannickcr/eslint-plugin-react
     rules: {
-        'react/boolean-prop-naming': 0, // We don't want to give special weight to `boolean`.
         'react/button-has-type': 1,
 
         'react/no-access-state-in-setstate': 1,
-        // The index of `Array<T>` is not suitable for `key` props.
-        // But this restriction does not prevent that the id for each items is just a sequence number of some list
-        // even if a item has an "unique" id. This rule cannot prevent it. meaningless.
-        'react/no-array-index-key': 0,
-        'react/no-adjacent-inline-elements': 0,
 
-        'react/no-multi-comp': 0, // Enable to define a multiple component to a single file.
-        'react/no-typos': 0,
         'react/no-unsafe': 2, // We should ban an unsafe operation.
-        'react/no-unused-prop-types': [
-            0,
-            {
-                // XXX: Disable to avoid mis-detection
-                customValidators: [],
-                skipShapeProps: false,
-            },
-        ],
-        // At v7.13, this only supports Flowtype.
-        'react/prefer-read-only-props': 'off',
-        'react/require-default-props': 0, // This does not resolve the essence of problem.
-        'react/require-optimization': [
-            0,
-            {
-                allowDecorators: [],
-            },
-        ],
         'react/self-closing-comp': [
             2,
             {
@@ -45,25 +20,12 @@ module.exports = {
                 html: false,
             },
         ],
-        // I seem this might be a problematic only for class component,
-        // But we lives in the era of hooks and almost properties which is targeted by this rule
-        // are classic (non-recommended in today) ones.
-        'react/static-property-placement': 'off',
         'react/void-dom-elements-no-children': 1,
 
-        'react/sort-prop-types': [
-            0,
-            {
-                // we cannot force alphabetical order to our old codebase.
-                callbacksLast: true,
-                requiredFirst: true,
-            },
-        ],
         'react/style-prop-object': 2,
 
         // JSX-specific rules
         'react/jsx-boolean-value': [2, 'always'], // Enforce to specify html's boolean type attribute.
-        'react/jsx-fragments': 'off', // I don't think enforce the style for this.
         'react/jsx-handler-names': [
             2,
             {
@@ -72,7 +34,6 @@ module.exports = {
                 checkLocalVariables: false,
             },
         ],
-        'react/jsx-max-depth': 0, // We should not restrict this by default.
         'react/jsx-no-constructed-context-values': 'warn',
         'react/jsx-no-literals': 1,
         'react/jsx-no-script-url': 2,
@@ -101,7 +62,5 @@ module.exports = {
                 exceptions: [],
             },
         ],
-        'react/jsx-sort-default-props': 0,
-        'react/jsx-sort-props': 0, // we cannot force alphabetical order to our old codebase, and this is meaningless.
     },
 };
