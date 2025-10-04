ea7ac112cdd01c1cbcfdf6dadce10e2beab5e982
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-10-05 02:42:40 +0900

    sidebar: Remove unused `BookmarkRepository.onRemovedObservable()`

--- packages/linkplaces/src/sidebar/repository/BookmarkRepository.ts
+++ packages/linkplaces/src/sidebar/repository/BookmarkRepository.ts
@@ -1,9 +1,7 @@
 import type { Repository } from '@linkplaces/foundation';
-import { BehaviorSubject, Subject, type Observable } from '@linkplaces/foundation/rx';
+import { BehaviorSubject, type Observable } from '@linkplaces/foundation/rx';
 import { getUnfiledBoolmarkFolder } from '@linkplaces/shared/bookmark';
 import type { BookmarkTreeNode, WebExtBookmarkService } from '@linkplaces/webext_types';
-
-type BookmarkId = string;
 
 export class BookmarkRepository implements Repository<Array<BookmarkTreeNode>> {
     static create(bookmarks: WebExtBookmarkService, init: Array<BookmarkTreeNode>): BookmarkRepository {
@@ -20,16 +18,11 @@ export class BookmarkRepository implements Repository<Array<BookmarkTreeNode>> {
         // bookmarks.onChildrenReordered.addListener(callback); // unimplemted in Fireofxma
         bookmarks.onMoved.addListener(callback);
         bookmarks.onCreated.addListener(callback);
-        bookmarks.onRemoved.addListener((id: BookmarkId, _info) => {
-            // eslint-disable-next-line no-underscore-dangle
-            s._onRemoved(id);
-            callback();
-        });
+        bookmarks.onRemoved.addListener(callback);
         return s;
     }
 
     private _subject: BehaviorSubject<Array<BookmarkTreeNode>>;
-    private _onRemoveSubject: Subject<BookmarkId> = new Subject();
 
     private constructor(init: Array<BookmarkTreeNode>) {
         this._subject = new BehaviorSubject(init);
@@ -44,19 +37,10 @@ export class BookmarkRepository implements Repository<Array<BookmarkTreeNode>> {
     }
 
     destroy(): void {
-        this._onRemoveSubject.unsubscribe();
         this._subject.unsubscribe();
     }
 
     asObservable(): Observable<Array<BookmarkTreeNode>> {
         return this._subject.asObservable();
-    }
-
-    private _onRemoved(id: BookmarkId): void {
-        this._onRemoveSubject.next(id);
-    }
-
-    onRemovedObservable(): Observable<BookmarkId> {
-        return this._onRemoveSubject.asObservable();
     }
 }
