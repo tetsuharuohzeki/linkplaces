007b499276c17dcc9ce92914c600e3786a9d1d57
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-10-07 08:08:36 +0900

    rx: `connect()` should take subject factory function because `Subject` will be dead after calling `.unsubscribe()` onca

--- packages/foundation/src/rx/operators/connect.ts
+++ packages/foundation/src/rx/operators/connect.ts
@@ -3,26 +3,29 @@ import { OperatorObservable, type OperatorFunction } from '../core/operator.js';
 import type { Subscriber } from '../core/subscriber.js';
 import type { Subject } from '../mod.js';
 
-class ConnectObservable<T> extends OperatorObservable<T, T> {
-    private _connector: Subject<T>;
+type SubjectFactoryFn<T> = () => Subject<T>;
 
-    constructor(source: Observable<T>, connector: Subject<T>) {
+class ConnectObservable<T> extends OperatorObservable<T, T> {
+    private _connector: SubjectFactoryFn<T>;
+
+    constructor(source: Observable<T>, connector: SubjectFactoryFn<T>) {
         super(source);
         this._connector = connector;
     }
 
     protected override onSubscribe(destination: Subscriber<T>): void {
-        this._connector.asObservable().subscribe(destination);
-        const sourceSub = this.source.subscribe(this._connector);
+        const connectorSubject = this._connector();
+        connectorSubject.asObservable().subscribe(destination);
+        const sourceSub = this.source.subscribe(connectorSubject);
         destination.addTeardown(() => {
             sourceSub.unsubscribe();
         });
     }
 }
 
-export function connect<T>(subject: Subject<T>): OperatorFunction<T, T> {
+export function connect<T>(subjectFactory: SubjectFactoryFn<T>): OperatorFunction<T, T> {
     const operator: OperatorFunction<T, T> = (source: Observable<T>) => {
-        const connected = new ConnectObservable<T>(source, subject);
+        const connected = new ConnectObservable<T>(source, subjectFactory);
         return connected;
     };
     return operator;
