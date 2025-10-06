b59cd1be7e644dc9644ad2a01990c1600092cafc
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-10-07 08:09:52 +0900

    BookmarkRepository should listens bookmark change events lazily

--- packages/linkplaces/src/sidebar/repository/BookmarkRepository.ts
+++ packages/linkplaces/src/sidebar/repository/BookmarkRepository.ts
@@ -1,31 +1,20 @@
 import type { Repository } from '@linkplaces/foundation';
-import { BehaviorSubject, type Observable } from '@linkplaces/foundation/rx';
+import { BehaviorSubject, createObservable, operators, type Observable } from '@linkplaces/foundation/rx';
 import { getUnfiledBoolmarkFolder } from '@linkplaces/shared/bookmark';
 import type { BookmarkTreeNode, WebExtBookmarkService } from '@linkplaces/webext_types';
 
 export class BookmarkRepository implements Repository<Array<BookmarkTreeNode>> {
     static create(bookmarks: WebExtBookmarkService, init: Array<BookmarkTreeNode>): BookmarkRepository {
-        const s = new BookmarkRepository(init);
-
-        const callback = () => {
-            getUnfiledBoolmarkFolder()
-                .then((list) => {
-                    s.next(list);
-                })
-                .catch(console.error);
-        };
-        bookmarks.onChanged.addListener(callback);
-        // bookmarks.onChildrenReordered.addListener(callback); // unimplemted in Fireofxma
-        bookmarks.onMoved.addListener(callback);
-        bookmarks.onCreated.addListener(callback);
-        bookmarks.onRemoved.addListener(callback);
+        const s = new BookmarkRepository(init, bookmarks);
         return s;
     }
 
     private _subject: BehaviorSubject<Array<BookmarkTreeNode>>;
+    private _observable: Observable<Array<BookmarkTreeNode>>;
 
-    private constructor(init: Array<BookmarkTreeNode>) {
+    private constructor(init: Array<BookmarkTreeNode>, bookmarks: WebExtBookmarkService) {
         this._subject = new BehaviorSubject(init);
+        this._observable = createOnChangeBookmarks(bookmarks).pipe(operators.connect(() => this._subject));
     }
 
     latestValue(): Array<BookmarkTreeNode> {
@@ -41,6 +30,32 @@ export class BookmarkRepository implements Repository<Array<BookmarkTreeNode>> {
     }
 
     asObservable(): Observable<Array<BookmarkTreeNode>> {
-        return this._subject.asObservable();
+        return this._observable;
     }
+}
+
+function createOnChangeBookmarks(bookmarks: WebExtBookmarkService): Observable<Array<BookmarkTreeNode>> {
+    const o = createObservable((destination) => {
+        const callback = () => {
+            getUnfiledBoolmarkFolder()
+                .then((list) => {
+                    destination.next(list);
+                })
+                .catch(console.error);
+        };
+        bookmarks.onChanged.addListener(callback);
+        // bookmarks.onChildrenReordered.addListener(callback); // unimplemted in Firefox
+        bookmarks.onMoved.addListener(callback);
+        bookmarks.onCreated.addListener(callback);
+        bookmarks.onRemoved.addListener(callback);
+
+        destination.addTeardown(() => {
+            bookmarks.onChanged.removeListener(callback);
+            // bookmarks.onChildrenReordered.removeListener(callback); // unimplemted in Firefox
+            bookmarks.onMoved.removeListener(callback);
+            bookmarks.onCreated.removeListener(callback);
+            bookmarks.onRemoved.removeListener(callback);
+        });
+    });
+    return o;
 }
