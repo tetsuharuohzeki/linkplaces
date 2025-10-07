6ffa9ba967788a43eda54b5ed76282019dc93a19
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-10-08 01:44:20 +0900

    foundation: Remove `ReactRuledViewContext`

--- packages/foundation/src/view_ctx/react_ruled_view_context.tsx
+++ packages/foundation/src/view_ctx/react_ruled_view_context.tsx
@@ -1,38 +1,7 @@
-import { type Nullable, isNotNull, expectNotNull } from 'option-t/nullable';
-
 import { StrictMode, type ReactNode } from 'react';
-import { type Root, type RootOptions, createRoot } from 'react-dom/client';
+import { type RootOptions, createRoot } from 'react-dom/client';
 
 import { getMountPoint } from './landing_pad.js';
-import type { ViewContext } from './view_context.js';
-
-export abstract class ReactRuledViewContext implements ViewContext {
-    private _renderRoot: Nullable<Root> = null;
-
-    abstract destroy(): void;
-
-    protected _initRenderRoot(mountpoint: Element): void {
-        if (isNotNull(this._renderRoot)) {
-            throw new TypeError('the react render root has been initialized');
-        }
-
-        this._renderRoot = createRoot(mountpoint);
-    }
-
-    protected _destroyRenderRoot(): void {
-        const renderRoot = expectNotNull(this._renderRoot, 'should has been initialized the renderRoot');
-        renderRoot.unmount();
-        this._renderRoot = null;
-    }
-
-    protected _getRenderRoot(): Root {
-        return expectNotNull(this._renderRoot, 'should has been initialized the renderRoot');
-    }
-
-    abstract onActivate(mountpoint: Element): Promise<void>;
-
-    abstract onDestroy(mountpoint: Element): Promise<void>;
-}
 
 export type ReactViewRenderFn = (view: ReactNode) => void;
 
