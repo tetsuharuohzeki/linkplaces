4b0ffe76e0d69cbbcaca9d40320c2831f6c17071
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-10-08 00:24:39 +0900

    foundation: Implement `renderReactView()`

--- packages/foundation/src/view_ctx/LandingPad.ts
+++ packages/foundation/src/view_ctx/LandingPad.ts
@@ -5,7 +5,12 @@ import type { ViewContext } from './ViewContext.js';
 const DOM_MOUNTPOINT_ID = 'js-mountpoint';
 
 export async function landViewContext(ctx: ViewContext): Promise<void> {
+    const mountpoint = getMountPoint(document);
+    await ctx.onActivate(mountpoint);
+}
+
+export function getMountPoint(document: Document): Element {
     const element = document.getElementById(DOM_MOUNTPOINT_ID);
     const mountpoint = expectNotNull(element, 'not found mountpoint');
-    await ctx.onActivate(mountpoint);
+    return mountpoint;
 }
--- packages/foundation/src/view_ctx/ReactRuledViewContext.ts
+++ packages/foundation/src/view_ctx/ReactRuledViewContext.ts
@@ -1,7 +1,9 @@
 import { type Nullable, isNotNull, expectNotNull } from 'option-t/nullable';
 
+import type { ReactNode } from 'react';
 import { type Root, createRoot } from 'react-dom/client';
 
+import { getMountPoint } from './LandingPad.js';
 import type { ViewContext } from './ViewContext.js';
 
 export abstract class ReactRuledViewContext implements ViewContext {
@@ -30,4 +32,24 @@ export abstract class ReactRuledViewContext implements ViewContext {
     abstract onActivate(mountpoint: Element): Promise<void>;
 
     abstract onDestroy(mountpoint: Element): Promise<void>;
+}
+
+export type ReactViewRenderFn = (view: ReactNode) => void;
+
+export type InitFn = (renderRoot: ReactViewRenderFn) => Promise<TeardownFn>;
+
+export type TeardownFn = () => void;
+
+export async function renderReactView(init: InitFn): Promise<TeardownFn> {
+    const mountpoint = getMountPoint(document);
+    const renderRoot = createRoot(mountpoint);
+    const render: ReactViewRenderFn = (view: ReactNode): void => {
+        renderRoot.render(view);
+    };
+    const teardown = await init(render);
+
+    return () => {
+        renderRoot.unmount();
+        teardown();
+    };
 }
