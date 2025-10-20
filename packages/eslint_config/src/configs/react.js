d0e39039a067a9b870b5a0af229774596146d65e
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-10-21 07:16:25 +0900

    Use `strict-typescript` preset for `@eslint-react/eslint-plugin`
    https://github.com/Rel1cx/eslint-react/releases/tag/v2.1.1

--- packages/eslint_config/src/configs/react.js
+++ packages/eslint_config/src/configs/react.js
@@ -43,7 +43,7 @@ export const reactConfigs = defineConfig([
 
     {
         files: FILE_PATTERN_TS,
-        extends: [eslintReact.configs['recommended-typescript']],
+        extends: [eslintReact.configs['strict-typescript']],
         rules: {
             // Enforce to specify html's boolean type attribute.
             '@eslint-react/jsx-shorthand-boolean': [
@@ -78,6 +78,8 @@ export const reactConfigs = defineConfig([
                 // See https://github.com/Rel1cx/eslint-react/blob/5d8e0c5f7f6ca254f66e3c59a9d24f4483431fda/packages/plugins/eslint-plugin-react-x/src/rules/jsx-shorthand-fragment.ts#L63-L84
                 1,
             ],
+
+            '@eslint-react/prefer-destructuring-assignment': 'off',
         },
     },
 
