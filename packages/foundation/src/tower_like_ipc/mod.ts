bd3aa63cb15cb741fc89a5034e853c3eeef5160a
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2026-02-25 20:28:29 +0900

    tower_like_ipc: Remove port related classes
    For manifest v3, background script cannot be persisted. We don't use them.

--- packages/foundation/src/tower_like_ipc/mod.ts
+++ packages/foundation/src/tower_like_ipc/mod.ts
@@ -1,21 +1,3 @@
-export { OneShotPacketResponder } from './port/one_shot_packet_responder.js';
-export { OnPortClientConnection } from './port/on_port_client_connection.js';
-export { OnPortServerConnection } from './port/on_port_server_connection.js';
-export {
-    type Packet,
-    type IdentifiablePacket,
-    type OneShotPacket,
-    createIdentifiablePacket,
-    isIdentifiablePacket,
-    createOneShotPacket,
-    isOneShotPacket,
-    assertPacket,
-    assertIdentifiablePacket,
-    assertOneShotPacket,
-    isPacket,
-} from './port/packet.js';
-export { PacketResponder } from './port/packet_responder.js';
-export { ReplyPacketResponder } from './port/reply_packet_responder.js';
 export type { MessageSendable } from './message_sendable.js';
 export { ToBackgroundMessageSender, MessageResponderSideError } from './send_message_sender.js';
 export { callResponderServiceWithMessage } from './send_message_responder.js';
